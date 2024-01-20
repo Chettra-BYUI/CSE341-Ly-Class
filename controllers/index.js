ff4fc3c2c086c62855798acbb51bf039a66047c7
Chettra Ly <Chettra@BYUI.edu>
2024-01-19 23:42:13 -0800

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const awesomeFunction = (req, res, next) => {
+    res.json('Dariya Ros')};
+
+const returnAnotherPerson = (req, res, next) => {
+    res.json('sumper Awesome person')};
+
+
+module.exports ={awesomeFunction, returnAnotherPerson};

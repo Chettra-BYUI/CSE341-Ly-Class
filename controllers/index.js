165a1431657863874a84f5b2368d388c1c8da973
Chettra Ly <Chettra@BYUI.edu>
2024-01-20 01:02:22 -0800

    small change

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const awesomeFunction = (req, res, next) => {
-    res.json('Dariya Ros')};
+    res.json('Dariya Ros, my love. You are perfect for me.')};
 
 const returnAnotherPerson = (req, res, next) => {
     res.json('sumper Awesome person')};
